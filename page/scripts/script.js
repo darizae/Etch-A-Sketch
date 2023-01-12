3a9aeeed6d015900feb92f37cf769c78c4cc7b59
darizae <danielarizae@gmail.com>
2023-01-12 20:01:03 +0100

    Create main selectors in JS script

---
+++ page/scripts/script.js
@@ -0,0 +1,19 @@
+
+//Selecting slider
+const gridSlider = document.querySelector("#slider");
+
+//Selecting color picker
+const colorPicker = document.querySelector("#color-picker");
+
+//Selecting settings section buttons
+const colorButton = document.querySelector("#color-button");
+const randomButton = document.querySelector("#random-button");
+const rainbowButton = document.querySelector("#rainbow-button");
+const shadowButton = document.querySelector("#shadow-button");
+
+//Selecting Erasing sections buttons
+const eraserButton = document.querySelector("#eraser-button");
+const clearButton = document.querySelector("#clear-button");
+
+//Selecting grid
+const gridSpace = document.querySelector("#drawing-section");

9353ebc7acd2ec7654547a2fbd5fef7b8b6d1109
darizae <danielarizae@gmail.com>
2023-01-13 14:00:30 +0100

    Style color picker

--- page/scripts/script.js
+++ page/scripts/script.js
@@ -4,6 +4,7 @@ const gridSlider = document.querySelector("#slider");
 
 //Selecting color picker
 const colorPicker = document.querySelector("#color-picker");
+var currentColor = colorPicker.value;
 
 //Selecting settings section buttons
 const colorButton = document.querySelector("#color-button");
@@ -22,6 +23,7 @@ const gridSpace = document.querySelector("#grid");
 var gridSize = gridSlider.value;
 
 buildGrid();
+buildColorPicker();
 
 function buildGrid() {
   
@@ -54,6 +56,37 @@ function addMouseListeners(gridDiv) {
         }
     });  
 }
+
+function buildColorPicker() {
+  updateColor(colorPicker.value);
+  colorPicker.addEventListener('input', (event) => {
+    updateColor(event.target.value);
+});
+
+function updateColor(color) {
+  currentColor = color;
+  colorPicker.style.backgroundColor = color;
+  colorPicker.style.borderColor = color;
+}
+/**
+ * To Do's:
+ * - Style color picker, preferably to a circle
+ * - Get value of color picker in JS
+ * - Refactor so color assignment is done from a variable in script, remove CSS rule    for coloring
+ * - Keep colored-class there for future use
+ * - Create numerical values for each coloring mode
+ * - Based on selected button, a variable is assigned the button's corresponding int
+ * - Current assigning color should be equal to color picker's value by default (0)
+ * - Upon newDiv creation (make its own function), the assigned background color style depends on the current variable, depending on it a switch function calls the function that generates that color
+ * - Make mouse icon change when in grid space
+ * - Implement eraser, like coloring but with color white
+ * - Clear canvas -> sets background color of all grid divs to white
+ * - Style slider
+ * - Implement slider input listener, clears and recalculates grid
+ * - Add a toggle that activates the grid having individual pixel delimination
+ * - This should be able to be activated by user
+ * - Activated when changing grid's dimensions -> deactivated upon starting to draw (not if the toggle is currently user-activated)
+ */
   
-  
+}
 
